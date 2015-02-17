678accc50b63fe455cf93f424a26718393de9e7f
Larry Jones <mrwizard82d1@gmail.com>
2015-02-17 01:49:55 -0600

    Wrote pmf/increment-mass.

--- think-bayes/src/think_bayes/pmf.clj
+++ think-bayes/src/think_bayes/pmf.clj
@@ -2,3 +2,6 @@
 
 (defn set-mass [pmf value mass]
   (assoc pmf value mass))
+
+(defn increment-mass [pmf value]
+  (assoc pmf value (inc (get pmf value 0))))
--- think-bayes/test/think_bayes/pmf_test.clj
+++ think-bayes/test/think_bayes/pmf_test.clj
@@ -1,5 +1,6 @@
 (ns think-bayes.pmf-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure [string :as string]
+             [test :refer :all]]
             [think-bayes.pmf :refer :all]))
 
 (deftest pmf-test
@@ -12,5 +13,10 @@
            (reduce #(set-mass %1 %2 (/ 1 %2)) {} (range 1 (inc 6))))))
   (testing "Non-empty PMF"
     (is (= {1 (/ 1 6) 2 (/ 1 3)}
-           (set-mass {1 (/ 1 6)} 2 (/ 1 3))
-           ))))
+           (set-mass {1 (/ 1 6)} 2 (/ 1 3)))))
+  (testing "increment-mass"
+    (is (= {"the" 2 "quick" 1 "little" 1 "brown" 1 "fox" 1
+            "jumped" 1 "over" 1 "lazy" 1 "grey" 1 "lambs" 1}
+           (reduce #(increment-mass %1 %2) {}
+                   (string/split "the quick little brown fox jumped over the lazy grey lambs"
+                          #"\s+"))))))
