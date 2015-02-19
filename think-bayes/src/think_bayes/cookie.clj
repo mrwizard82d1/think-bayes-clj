aa8403884205d32b02278c2bf6616978303ec939
Larry Jones <mrwizard82d1@gmail.com>
2015-02-19 09:58:51 -0600

    Wrote cookie/update.

--- think-bayes/src/think_bayes/cookie.clj
+++ think-bayes/src/think_bayes/cookie.clj
@@ -7,3 +7,24 @@
   ([] {})
   ([hypotheses]
    (reduce #(pmf/set-mass %1 %2 1) {} hypotheses)))
+
+#_(defn update-hypothesis-mass
+  [to-update-pmf hypothesis data]
+  (pmf/multiply-mass to-update-pmf hypothesis
+                     (likelihood data hypothesis)))
+
+#_(defn update
+  "Update the prior probabilities based on data and likelihood."
+  [prior data likelihood]
+    (reduce #(%1 %2 data) prior (keys prior)))
+
+(defn update-hypothesis-mass
+  "Update the probability mass of a single hypothesis."
+  [pmf likelihood data hypothesis]
+    (pmf/multiply-mass pmf hypothesis (likelihood data hypothesis)))
+
+(defn update
+  "Update a prior PMF based on the likelihood of seeing data."
+  [prior data likelihood]
+    (reduce #(update-hypothesis-mass %1 likelihood data %2)
+            prior (keys prior)))
--- think-bayes/test/think_bayes/cookie_test.clj
+++ think-bayes/test/think_bayes/cookie_test.clj
@@ -1,18 +1,32 @@
 (ns think-bayes.cookie-test
   (:require (clojure [test :refer :all])
-            [think-bayes.cookie :as cookie]))
+            [think-bayes.cookie :as cookie]
+            [think-bayes.pmf :as pmf]))
 
 (deftest cookie-test
   (testing "No hypotheses"
     (let [no-hypothesis-cookie (cookie/init)]
       (testing "Empty PMF"
-        (is (empty? no-hypothesis-cookie)))))
+        (is (empty? no-hypothesis-cookie))
+        (is (empty? (cookie/update no-hypothesis-cookie
+                                   :unused :unused))))))
   (testing "A single hypothesis"
     (let [single-hypothesis-cookie (cookie/init ["lorem"])]
       (testing "PMF has single hypothesis with weight 1"
-        (is (= {"lorem" 1} single-hypothesis-cookie)))))
+        (is (= {"lorem" 1} single-hypothesis-cookie)))
+      (testing "Posterior has weight 2"
+        (is (= {"lorem" 2}
+               (cookie/update single-hypothesis-cookie
+                              :unused (fn [_d _h] 2)))))))
   (testing "Many hypotheses"
     (let [many-hypotheses-cookie (cookie/init (range 1 7))]
       (testing "PMF has many hypotheses all with weight 1"
         (is (= (zipmap (range 1 7) (repeat 6 1))
-              many-hypotheses-cookie))))))
+              many-hypotheses-cookie)))
+      (testing "Update and posterior has correct weights"
+        (is (= 1/6
+               (pmf/get-mass
+                 (cookie/update many-hypotheses-cookie
+                                3
+                                (fn [_d _h] 1/6))
+                 3)))))))
