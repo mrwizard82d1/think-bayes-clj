1bb886e58c12f16c81c99676aa5e6bc543f57540
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2016-04-23 08:52:14 -0500

    Add unit tests for pmf/mass.

--- think-bayes-clj/test/think_bayes/pmf_test.clj
+++ think-bayes-clj/test/think_bayes/pmf_test.clj
@@ -2,7 +2,7 @@
   (:require [clojure.test :as t]
             [think-bayes.pmf :as pmf]))
 
-(t/deftest pmf-make
+(t/deftest make-test
   (t/are [e s] (= e (pmf/make s))
     {} []
     {:a 1} #{:a}
@@ -11,3 +11,13 @@
      "senis" 2
      "retis" 1} (clojure.string/split "glabra glabra senis recidiva glabra recidiva senis glabra recidiva glabra retis" #"\s+")))
  
+(t/deftest mass-test
+  (let [empty-pmf {}
+        single-pmf {:a 7}
+        many-pmf {:glabra 5 :recidiva 3 :senis 2 :retis 1}]
+    (t/are [e p m] (= e (pmf/mass p m))
+      0 empty-pmf :a
+      7 single-pmf :a
+      0 single-pmf :b
+      5 many-pmf :glabra
+      0 many-pmf "glabra")))
