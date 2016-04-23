7c19f58b3bc6a52be5a0f7236faac23d3eb9d4c1
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2016-04-22 19:29:38 -0500

    Add first unit test.
    Because I cannot get CIDER to work, I have chosen to not pursue this
    effort in ClojureScript. I may come back to it at some point, but I
    **miss** the convenience of a single environment.
    
    Perhaps I'll try Cursive again. Hmmm...
    
    Tried Cursive. Still could not do what I wanted. Sigh...

--- think-bayes-cljs/src/think_bayes/pmf.cljs
+++ think-bayes-cljs/src/think_bayes/pmf.cljs
@@ -1,13 +1,24 @@
 (ns think-bayes.pmf)
 
-(defn make 
-  "Makes a PMF (probability mass function.
+(defn mass
+  "Returns the mass of the value, v, in the probability mass function, pmf."
+  [pmf v]
+  (pmf v 0))
 
-   A PMF is useful for modeling discrete probability distributions that **may not** be normalized."
-  []
-  {})
+(defn transform
+  "Transforms the probability of the value, v, using a transformation function, f.
 
-(defn set
-  "Sets the probability mass of an event to value in pmf."
-  [pmf event mass]
-  (assoc pmf event mass))
+  f is a function of a single argument, the untransformed probability."
+  [pmf v f]
+  (assoc pmf v (f (mass pmf v))))
+
+(defn make
+  "Construct a probability mass function from a sequence, s.
+
+  A probability mass function (or PMF) maps discrete values to masses. (See 
+  https://en.wikipedia.org/wiki/Probability_mass_function for additional details.)
+
+  In this particular implementation, the mass of a value is only a probability if the masses have been normalized."
+  [values]
+  (reduce #(transform %1 %2 inc) {} values))
+
