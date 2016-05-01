0d7da6468a17bc6f087d20c4a651e9e3af128ea3
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2016-05-01 18:50:24 -0500

    Write 'Monty Python' model

--- think-bayes-clj/test/think_bayes/core_test.clj
+++ think-bayes-clj/test/think_bayes/core_test.clj
@@ -1,8 +1,9 @@
 (ns think-bayes.core-test
   (:require [clojure.test :as t]
-            [think-bayes pmf-test table-test cookie-test]))
+            [think-bayes pmf-test table-test cookie-test monty-python-test]))
 
 (t/run-tests 'think-bayes.pmf-test
              'think-bayes.table-test
-             'think-bayes.cookie-test)
+             'think-bayes.cookie-test
+             'think-bayes.monty-python-test)
 
