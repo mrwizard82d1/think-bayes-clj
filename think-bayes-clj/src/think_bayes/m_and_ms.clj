8625a792f3ad8e2d296e20520fee31f261ca3cb8
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2016-05-06 19:49:04 -0500

    Modeled probabilities of picking M&Ms.

--- think-bayes-clj/src/think_bayes/m_and_ms.clj
+++ think-bayes-clj/src/think_bayes/m_and_ms.clj
@@ -8,20 +8,6 @@
   (-> (pmf/make hypotheses)
       pmf/normalize))
 
-(defn likelihood [d h]
-  (let [mix {:bag1 {:brown 30
-                    :yellow 20
-                    :red 20
-                    :green 10
-                    :orange 10}
-             :bag2 {:blue 24
-                    :green 20
-                    :orange 16
-                    :yellow 14
-                    :red 13
-                    :brown 13}}]
-    (get-in mix [h d])))
-
 (defn other
   "Calculate the 'other' hypothesis of a pair of hypotheses."
   [pmf h]
@@ -30,10 +16,11 @@
 (defn posterior
   "Calculate the posterior given a prior PMF, datum and a likelihood.
 
-  The datum is different from other datums. Each datum is a pair of colors; the 'difficulty'
-  is that we do not know from which bag each color was drawn. As a result, a pair, 
-  [:yellow :green], has a mass of 20*20 if bag 1 is from 1994 (and bag 2 is from 1996). And
-  the pair [:yellow :green] has a mass of 14*20 if bag 1 is from 1996 and bag 2 is from 1994."
+  The datum is different from other datums. Each datum is a pair of colors; the 
+  'difficulty' is that we do not know from which bag each color was drawn. As a 
+  result, a pair, [:yellow :green], has a mass of 20*20 if bag 1 is from 1994 
+  (and bag 2 is from 1996). And the pair [:yellow :green] has a mass of 14*20 if 
+  bag 1 is from 1996 and bag 2 is from 1994."
   [pmf datum likelihood]
   (pmf/normalize (reduce #(->> (* (likelihood (first datum) %2)
                                   (likelihood (second datum) (other pmf %2)))
--- think-bayes-clj/test/think_bayes/m_and_ms_test.clj
+++ think-bayes-clj/test/think_bayes/m_and_ms_test.clj
@@ -3,6 +3,24 @@
             [think-bayes.m-and-ms :as m-and-ms]
             [think-bayes.pmf :as pmf]))
 
+(defn likelihood [d h]
+  (let [mix {:bag1 {:brown 30
+                    :yellow 20
+                    :red 20
+                    :green 10
+                    :orange 10
+                    :tan 10}
+             :bag2 {:blue 24
+                    :green 20
+                    :orange 16
+                    :yellow 14
+                    :red 13
+                    :brown 13}}]
+    ;; In other similar situations, I've chosen to *NOT* return 0; however,
+    ;; because I expect that clients may request the likelihood of a blue
+    ;; M&M from bag1 or the likelihood of a tan M&M from bag2.
+    (get-in mix [h d] 0)))
+
 (let [m-and-ms-pmf (m-and-ms/make [:bag1 :bag2])]
   (t/deftest make-test
     (t/are [e v]
@@ -11,11 +29,27 @@
            (/ 1 2) :bag2))
   (let [posterior (m-and-ms/posterior m-and-ms-pmf
                                       [:yellow :green] 
-                                      m-and-ms/likelihood)]
-    (t/deftest make-test
+                                      likelihood)]
+    (t/deftest posterior-test
       (t/are [e h]
              (= e (pmf/probability posterior h))
              (/ 20 27) :bag1
-             (/ 7 27) :bag2))))
+             (/ 7 27) :bag2)))
+  (let [posterior (m-and-ms/posterior m-and-ms-pmf
+                                      [:yellow :blue] 
+                                      likelihood)]
+    (t/deftest posterior-test
+      (t/are [e h]
+             (= e (pmf/probability posterior h))
+             1 :bag1
+             0 :bag2)))
+  (let [posterior (m-and-ms/posterior m-and-ms-pmf
+                                      [:green :red] 
+                                      likelihood)]
+    (t/deftest posterior-test
+      (t/are [e h]
+             (= e (pmf/probability posterior h))
+             (/ 13 53) :bag1
+             (/ 40 53) :bag2))))
 
 
