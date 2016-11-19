1a95d926c33d302ab0604a1492553d77caa6eba4
Larry Jones <larryj@blinds.com>
2016-11-18 22:02:41 -0600

    Changed definition of total probabilty
    Previous commits simply used the term total probability. This commit
    used the definition of total probability: the weighted average of the
    probabilites of the data (vanilla) given different hypotheses (bowl 1
    and bowl 2). The weights are the probabilities of each hypothesis.

--- think-bayes-clj/src/think_bayes/cookies.clj
+++ think-bayes-clj/src/think_bayes/cookies.clj
@@ -6,8 +6,11 @@
 
 (defn probability-bowl-1-given-vanilla []
   (let [probability-bowl-1 (/ 1 2)
+        probability-bowl-2 (/ 1 2)
         probability-vanilla-given-bowl-1 (/ 3 4)
-        total-probability (/ 5 8)]
+        probability-vanilla-given-bowl-2 (/ 1 2)
+        total-probability (+ (* probability-bowl-1 probability-vanilla-given-bowl-1)
+                             (* probability-bowl-2 probability-vanilla-given-bowl-2))]
     (bayes-law probability-bowl-1 probability-vanilla-given-bowl-1 total-probability)))
 
 
