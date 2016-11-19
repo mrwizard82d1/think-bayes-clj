bec8e7e66d562da9b14d87febdf018063a3c2acb
Larry Jones <larryj@blinds.com>
2016-11-19 16:05:29 -0600

    Implement one "Monty Hall" variation
    In this variation, Monty Hall randomly picks one of the doors that the
    contestant **did not** pick (but does **not** reveal the car no matter
    what).

---
+++ think-bayes-clj/src/think_bayes/monty_hall.clj
@@ -0,0 +1,21 @@
+(ns think-bayes.monty-hall
+  (require [think-bayes.core :as core]))
+
+(defn should-switch-if-monty-picks-random
+  "Solve the 'Monty Hall' problem.
+
+   In this problem, Monty Hall offers 3 doors: one of which has a very valuable prize and two of which not so much. You pick one. He
+   then shows you one of the doors you **did not** pick. (He selects this door randomly.) Should you switch?"
+  []
+  (let [hypotheses [:car-behind-a :car-behind-b :car-behind-c]
+        p-hypothesis (reduce #(assoc %1 %2 (/ 1 3)) {} hypotheses)
+        p-data-given-hypothesis (fn [[datum hypothesis]]
+                                  (cond
+                                    (and (= hypothesis :car-behind-a)
+                                         (= (first datum) :open-b)) (/ 1 2)
+                                    (and (= hypothesis :car-behind-b)
+                                         (= (first datum) :open-b)) 0
+                                    (and (= hypothesis :car-behind-c)
+                                         (= (first datum) :open-b)) 1))
+        posteriors (core/posterior hypotheses [:open-b :no-car] p-hypothesis p-data-given-hypothesis)]
+    (posteriors :car-behind-c)))
