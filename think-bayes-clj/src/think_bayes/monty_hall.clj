2da1ffcfff819fc5118c3eeea9d60275ddfe3cb8
Larry Jones <larryj@blinds.com>
2016-11-19 17:11:19 -0600

    Implement another "Monty Hall" variation
    In this variation, Monty Hall always picks door "B" if he can.
    
    The interesting consequence of this choice is to reveal no additional
    information to the contestant.

--- think-bayes-clj/src/think_bayes/monty_hall.clj
+++ think-bayes-clj/src/think_bayes/monty_hall.clj
@@ -19,3 +19,22 @@
                                          (= (first datum) :open-b)) 1))
         posteriors (core/posterior hypotheses [:open-b :no-car] p-hypothesis p-data-given-hypothesis)]
     (posteriors :car-behind-c)))
+
+(defn should-switch-if-monty-always-picks-b
+  "Solve the 'Monty Hall' problem.
+
+   In this problem, Monty Hall offers 3 doors: one of which has a very valuable prize and two of which not so much. You pick one. He
+   then shows you one of the doors you **did not** pick. (He selects this door randomly.) Should you switch?"
+  []
+  (let [hypotheses [:car-behind-a :car-behind-b :car-behind-c]
+        p-hypothesis (reduce #(assoc %1 %2 (/ 1 3)) {} hypotheses)
+        p-data-given-hypothesis (fn [[datum hypothesis]]
+                                  (cond
+                                    (and (= hypothesis :car-behind-a)
+                                         (= (first datum) :open-b)) 1
+                                    (and (= hypothesis :car-behind-b)
+                                         (= (first datum) :open-b)) 0
+                                    (and (= hypothesis :car-behind-c)
+                                         (= (first datum) :open-b)) 1))
+        posteriors (core/posterior hypotheses [:open-b :no-car] p-hypothesis p-data-given-hypothesis)]
+    (posteriors :car-behind-c)))
