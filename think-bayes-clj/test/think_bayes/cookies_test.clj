657a0ef05c4b8342210cfcf0b7680fb4091e68d7
Larry Jones <larryj@blinds.com>
2016-11-22 09:57:04 -0600

    "Hand-crafted" cookie solution using `pmf`
    This commit illustrates a "hand-crafted" solution to the cookie problem
    using the `pmf` module.

--- think-bayes-clj/test/think_bayes/cookies_test.clj
+++ think-bayes-clj/test/think_bayes/cookies_test.clj
@@ -1,7 +1,18 @@
 (ns think-bayes.cookies-test
   (:require [clojure.test :refer :all]
-            [think-bayes.cookies :refer :all]))
+            [think-bayes.cookies :refer :all]
+            [think-bayes.pmf :as pmf]))
 
 (deftest cookies-test
   (testing "Verify p('Bowl 1' | 'vanilla cookie'"
-    (is (= (/ 3 5) (probability-bowl-1-given-vanilla)))))
+    (is (= (/ 3 5) (probability-bowl-1-given-vanilla))))
+  (testing "Verify p('Bowl 1' | 'vanilla cookie'"
+    (let [priors (-> {}
+                     (pmf/set-probability "Bowl 1" (/ 1 2))
+                     (pmf/set-probability "Bowl 2" (/ 1 2)))
+          unnormalized (-> priors
+                           (pmf/scale "Bowl 1" (/ 3 4))
+                           (pmf/scale "Bowl 2" (/ 1 2)))
+          posteriors (pmf/normalize unnormalized)]
+      (is (= (/ 3 5) (pmf/probability posteriors "Bowl 1")))
+      (is (= (/ 2 5) (pmf/probability posteriors "Bowl 2"))))))
