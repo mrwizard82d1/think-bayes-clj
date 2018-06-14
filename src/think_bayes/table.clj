39056ffa2adb876f8f162e8ae9182d253bd4d452
Larry Jones <larry.jones@reveal-energy.com>
2018-06-13 22:56:49 -0500

    Implement Monty Hall problem using table method

--- src/think_bayes/table.clj
+++ src/think_bayes/table.clj
@@ -1,6 +1,6 @@
 (ns think-bayes.table)
 
-(defn likelihood [data hypothesis & pairs]
+(defn mnm-likelihood [data hypothesis & pairs]
   (let [mnm-mix-1994 {:brown 30
                       :yellow 20
                       :red 20
@@ -24,10 +24,31 @@
         mnm-priors {:mnm-mix-1994 (/ 1 2)
                     :mnm-mix-1996 (/ 1 2)}
         priors (map mnm-priors mnm-hypotheses)
-        likelihoods [(apply likelihood (interleave colors mnm-hypotheses))
-                     (apply likelihood (interleave (reverse colors) mnm-hypotheses))]
+        likelihoods [(apply mnm-likelihood (interleave colors mnm-hypotheses))
+                     (apply mnm-likelihood (interleave (reverse colors) mnm-hypotheses))]
         products (map * priors likelihoods)
         normalizing-constant (reduce + products)
         posteriors (zipmap mnm-hypotheses (map #(/ %1 normalizing-constant) products))]
+    posteriors))
+
+(defn monty-hall-likelihood [data hypothesis & pairs]
+  (let [monty-hall-pmf {:behind-a {:open-b (/ 1 2)}
+                        :behind-b {:open-b 0}
+                        :behind-c {:open-b 1}}]
+    (reduce * 
+            (get-in monty-hall-pmf [hypothesis data])
+            (map #(get-in monty-hall-pmf (reverse %1)) (partition 2 pairs)))))
+
+(defn monty-hall-posteriors [door]
+  (let [monty-hall-hypotheses #{:behind-a :behind-b :behind-c}
+        monty-hall-priors {:behind-a (/ 1 3)
+                           :behind-b (/ 1 3)
+                           :behind-c (/ 1 3)}
+        priors (map monty-hall-priors monty-hall-hypotheses)
+        likelihoods (map #(monty-hall-likelihood door %) monty-hall-hypotheses)
+        products (map * priors likelihoods)
+        normalizing-constant (reduce + products)
+        posteriors (zipmap monty-hall-hypotheses 
+                           (map #(/ %1 normalizing-constant) products))]
     posteriors))
 
--- test/think_bayes/table_test.clj
+++ test/think_bayes/table_test.clj
@@ -3,6 +3,10 @@
             [think-bayes.table :as tbt]))
 
 (facts "Bayes' table method"
-  (fact "M&M problem' table method"
+  (fact "M&M problem"
     (tbt/mnm-posteriors [:yellow :green]) => {:mnm-mix-1994 (/ 20 27)
-                                              :mnm-mix-1996 (/ 7 27)}))
+                                              :mnm-mix-1996 (/ 7 27)})
+  (fact "Monty Hall problem"
+    (tbt/monty-hall-posteriors :open-b) => {:behind-a (/ 1 3),
+                                            :behind-b 0,
+                                            :behind-c (/ 2 3)}))
