bff24bdadaeffcbc51f065314773f2e12beaa694
Larry Jones <larry.jones@reveal-energy.com>
2018-06-14 17:33:29 -0500

    Add `pmf/set-probabilities(pmf, value-prob-map)`

--- src/think_bayes/pmf.clj
+++ src/think_bayes/pmf.clj
@@ -1,4 +1,9 @@
 (ns think-bayes.pmf)
 
 (defn set-probability [pmf value probability]
+  "Set the `probability` of `value` in `pmf`." 
   (assoc pmf value probability))
+
+(defn set-probabilities [pmf value-probability-map]
+  "Set the probability of multiple values (the keys of `value-probability-map`) to the values of `value-probability-map`."
+  (merge pmf value-probability-map))
--- test/think_bayes/pmf_test.clj
+++ test/think_bayes/pmf_test.clj
@@ -6,4 +6,9 @@
   (fact "Set single probability for single value."
     ((pmf/set-probability {} :a (/ 1 6)) :a) => (/ 1 6)
     ((pmf/set-probability {} :a (/ 1 6)) :b) => nil
-    ((pmf/set-probability {:a (/ 1 5)} :a (/ 1 6)) :a) => (/ 1 6)))
+    ((pmf/set-probability {:a (/ 1 5)} :a (/ 1 6)) :a) => (/ 1 6))
+  (fact "Set probabilities for multiple values."
+    ((pmf/set-probabilities {} (zipmap (range 3) (repeat (/ 1 6)))) 2) => (/ 1 6)
+    ((pmf/set-probabilities {} (zipmap (range 3) (repeat (/ 1 6)))) 3) => nil
+    ((pmf/set-probabilities (zipmap (range 3) (repeat (/ 1 5)))
+                            (zipmap (range 3) (repeat (/ 1 6)))) 2) => (/ 1 6)))
