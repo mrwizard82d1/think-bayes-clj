ccc04bca568e2a92f6b8a37a0828b98fe5e76ca0
Larry Jones <larry.jones@reveal-energy.com>
2018-06-15 22:04:08 -0500

    Write pmf/normalize

--- src/think_bayes/pmf.clj
+++ src/think_bayes/pmf.clj
@@ -7,3 +7,15 @@
 (defn set-probabilities [pmf value-probability-map]
   "Set the probability of multiple values (the keys of `value-probability-map`) to the values of `value-probability-map`."
   (merge pmf value-probability-map))
+
+(defn increase
+  ([pmf value]
+   "Increase the probability mass of `value` by one."
+   (increase pmf value 1))
+  ([pmf value by]
+   "Increase the probability mass of `value` by `by`."
+   (assoc pmf value (+ (get pmf value 0) by))))
+
+(defn normalize [pmf]
+  "Normalize this pmf into a probablity density function."
+  (reduce (fn [m [k v]] (assoc m k (/ v (count (keys pmf))))) {} pmf))
--- test/think_bayes/pmf_test.clj
+++ test/think_bayes/pmf_test.clj
@@ -1,5 +1,6 @@
 (ns think-bayes.pmf-test
   (:require [midje.sweet :refer :all]
+            [clojure.string :as cstr]
             [think-bayes.pmf :as pmf]))
 
 (facts "Pmf (Probability mass function)"
@@ -11,4 +12,24 @@
     ((pmf/set-probabilities {} (zipmap (range 3) (repeat (/ 1 6)))) 2) => (/ 1 6)
     ((pmf/set-probabilities {} (zipmap (range 3) (repeat (/ 1 6)))) 3) => nil
     ((pmf/set-probabilities (zipmap (range 3) (repeat (/ 1 5)))
-                            (zipmap (range 3) (repeat (/ 1 6)))) 2) => (/ 1 6)))
+                            (zipmap (range 3) (repeat (/ 1 6)))) 2) => (/ 1 6))
+  (fact "Increment the probability mass of value."
+    ((pmf/increase {} :planum) :planum) => 1
+    ((pmf/increase {:maritum 5} :maritum 7) :maritum) => 12)
+  (fact "Normalize a probability mass to a probability density."
+    (let [word-list 
+          (cstr/split "The quick little brown fox jumped over the lazy grey lambs" 
+                      #"\s") 
+          pmf (->> word-list
+                  (map cstr/lower-case)
+                  (reduce pmf/increase {}))]
+      (pmf/normalize pmf) => {"the" (/ 2 10)
+                              "quick" (/ 1 10)
+                              "little" (/ 1 10)
+                              "brown" (/ 1 10)
+                              "fox" (/ 1 10)
+                              "jumped" (/ 1 10)
+                              "over" (/ 1 10)
+                              "lazy" (/ 1 10)
+                              "grey" (/ 1 10)
+                              "lambs" (/ 1 10)})))
