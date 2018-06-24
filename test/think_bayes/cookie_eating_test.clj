9f7c945a629987030f6840df573a82dd07d1e783
Larry Jones <larry.jones@reveal-energy.com>
2018-06-24 13:59:24 -0500

    Refactor test code to prepare for other tests

--- test/think_bayes/cookie_eating_test.clj
+++ test/think_bayes/cookie_eating_test.clj
@@ -17,23 +17,23 @@
   (update-in mixture [bowl flavor] dec))
 
 (facts "The cookie problem - but I eat cookies when picked."
-       (fact "Draw two vanilla cookies from bowl-1 with probability (/ 29 42)."
-             (let [priors (pmf/suite [:bowl-1 :bowl-2])
-                   bowl-1 {:vanilla 30, :chocolate 10}
-                   bowl-2 {:vanilla 20, :chocolate 20}
-                   mixes (make-mixes bowl-1 bowl-2)
-                   likelihood (make-likelihood mixes)
-                   posteriors-vanilla-bowl-1 (pmf/posteriors priors
-                                                             likelihood
-                                                             :vanilla)
-                   eat-vanilla-bowl-1-mixes (eat-cookie mixes
-                                                        :bowl-1
-                                                        :vanilla)
-                   likelihood-post-vanilla (make-likelihood eat-vanilla-bowl-1-mixes)
-                   posteriors
-                   (pmf/posteriors posteriors-vanilla-bowl-1
-                                   likelihood-post-vanilla
-                                   :vanilla)]
-               (pmf/probability posteriors :bowl-1) => (/ 29 42)
-               (pmf/probability posteriors :bowl-2) => (/ 13 42))))
+       (let [priors (pmf/suite [:bowl-1 :bowl-2])
+             bowl-1 {:vanilla 30, :chocolate 10}
+             bowl-2 {:vanilla 20, :chocolate 20}
+             mixes (make-mixes bowl-1 bowl-2)
+             likelihood (make-likelihood mixes)
+             posteriors-vanilla-bowl-1 (pmf/posteriors priors
+                                                       likelihood
+                                                       :vanilla)
+             eat-vanilla-bowl-1-mixes (eat-cookie mixes
+                                                   :bowl-1
+                                                   :vanilla)]
+         (fact "Draw two vanilla cookies from bowl-1 with probability (/ 29 42)."
+               (let [likelihood-post-vanilla
+                     (make-likelihood eat-vanilla-bowl-1-mixes)
+                     posteriors (pmf/posteriors posteriors-vanilla-bowl-1
+                                                likelihood-post-vanilla
+                                                :vanilla)]
+                 (pmf/probability posteriors :bowl-1) => (/ 29 42)
+                 (pmf/probability posteriors :bowl-2) => (/ 13 42)))))
 
